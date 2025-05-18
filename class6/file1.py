176087cdfcf423fab0a69579e3a6cd7885f0e07d
HamzaACCA <hamza.992@hotmail.com>
2025-05-18 12:46:30 +0000

    Class end

--- class6/file1.py
+++ class6/file1.py
@@ -1,4 +1,27 @@
-def greet(name: str, age: int) -> str :
-    return f"Hello, {name} {age}"
-abc = greet("Hamza", 22)
-print (abc)
+# def greet(name: str, age: int) -> str :
+#     return f"Hello, {name} {age}"
+# abc = greet("Hamza", 22)
+# print (abc)
+
+# studentDetails = {
+#     "Name": "Hamza",
+#     "Age": 21,
+#     "Roll No": 123
+# }
+
+# print(f"""
+#       Name = {studentDetails['Name']}
+#       Age = {studentDetails['Age']}
+#       Roll No = {studentDetails['Roll No']}
+# """)
+
+
+def llm (prompt: str):
+    return {
+        "content": "Hello, How can I help you",
+        "role": "assistant"
+
+    }
+response_dict = llm(prompt="Hi")
+
+print(response_dict["content"])
